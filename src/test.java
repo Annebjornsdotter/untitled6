89f8a6329c3df8eba54407909605d413ddb05120
Annebjornsdotter <annebjornsdotter87@gmail.com>
2022-02-16 16:12:09 +0100

    test one

--- src/test.java
+++ src/test.java
@@ -1,6 +1,12 @@
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("hej");
+        System.out.println(add(13,16));
+    }
+    //
+    public static int add(int a,int b) {
+        return a + b;
+
     }
 }
+
